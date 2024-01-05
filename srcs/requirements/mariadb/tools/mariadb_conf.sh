998df677f24b6d5fa2be66343bfd66f0cc7d26bf
Eunsol Kim <eunskim@student.42heilbronn.de>
2024-01-05 12:23:20 +0100

    mariadb working

--- srcs/requirements/mariadb/tools/mariadb_conf.sh
+++ srcs/requirements/mariadb/tools/mariadb_conf.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# create database and user, set a root password, grant privileges
+# Create database and user, set a root password, grant privileges
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 	mariadbd --user=mysql --bootstrap --console <<EOSQL
+USE mysql;
+FLUSH PRIVILEGES;
 CREATE DATABASE ${MYSQL_DATABASE};
 CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
